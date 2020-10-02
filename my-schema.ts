f6332b4f78b03d1a3e9e80083747d973d729358c
gosho-kazuya <ketsume0211@gmail.com>
2020-10-02 22:47:59 +0900

    init

---
+++ my-schema.ts
@@ -0,0 +1,39 @@
+import {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql';
+import {pubsub} from './pubsub'
+
+export const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve() {
+          return 'world';
+        },
+      },
+    },
+  }),
+  subscription: new GraphQLObjectType({
+    name: 'RootSubscriptionType',
+    fields: {
+      status: {
+        type: GraphQLString,
+        subscribe() {
+          return pubsub.asyncIterator('status')
+        },
+      },
+    },
+  }),
+});
+
+let count = 0
+setInterval(() => {
+  count++
+  const status = count % 2 === 0 ? 'online' : 'offline'
+  pubsub.publish('status', { status })
+}, 1500)
